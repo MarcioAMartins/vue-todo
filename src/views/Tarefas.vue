604e0b543c9d40c6fdb06595098fcfd9321ec41c
Marcio Alves <MarcioApocallypse@gmail.com>
2022-02-16 22:39:51 -0300

    Add logica inserir tarefa

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,44 +1,56 @@
 <template>
   <div>
-    <v-list
-      flat
-      subheader
-    >
-
-      <v-list-item-group
-        v-model="settings"
-        multiple
-        active-class=""
-      >
-      <div
-      v-for="tarefa, index in tarefas"
-      :key="index"
-      >
-      <Tarefa 
-       :tarefa="tarefa"
-      />
-      </div>
-
-
+    <v-col 
+     cols="12"
+     sm="6">
+      <v-text-field
+        v-model="campoInput"
+        label="Insira uma tarefa"
+        outlined
+        clearable
+        @keyup.enter="handleAddTarefa"
+      ></v-text-field>
+    </v-col>
+    <v-list flat subheader>
+      <v-list-item-group 
+      multiple active-class="">
+        <div 
+        v-for="(tarefa, index) in tarefas" 
+        :key="index">
+          <Tarefa :tarefa="tarefa" />
+        </div>
       </v-list-item-group>
     </v-list>
   </div>
 </template>
 
 <script>
-import Tarefa from '../components/tarefas/Tarefa.vue'
-  export default {
-    name: 'Home',
+import Tarefa from "../components/tarefas/Tarefa.vue";
+export default {
+  name: "Home",
 
-    components: {
-      Tarefa,
-    },
-    data(){
-      return{
-        tarefas:[
-          {titulo:"Ir ao mercado", concluido:false},
-        ]
+  components: {
+    Tarefa,
+  },
+  data() {
+    return {
+      campoInput: null ,
+      tarefas: [
+        { titulo: "Ir ao mercado", concluido: false },
+        { titulo: "Comprar ração", concluido: false },
+      ],
+    };
+  },
+  methods:{
+    handleAddTarefa(){
+      if(this.campoInput){
+        this.tarefas.push({
+          titulo:this.campoInput,
+          concluido: false
+        })
+        this.campoInput = null;
       }
     }
   }
+};
 </script>
