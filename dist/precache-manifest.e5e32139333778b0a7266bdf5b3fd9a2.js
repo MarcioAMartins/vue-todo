3d97eec18f8c89b7e5dcb875c8668adf5acf8772
Marcio Alves <MarcioApocallypse@gmail.com>
2022-03-01 09:59:17 -0300

    Upando Dist

---
+++ dist/precache-manifest.e5e32139333778b0a7266bdf5b3fd9a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fad4d0203c7d0ec1d24",
+    "url": "/css/chunk-17bc2355.c479b06d.css"
+  },
+  {
+    "revision": "37cd74c837945674e8d6",
+    "url": "/css/chunk-vendors.2973f4f6.css"
+  },
+  {
+    "revision": "f3e5a3fef5242032da64c878500852e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3ebc74f672f58f335ed",
+    "url": "/js/app.4fbb2cea.js"
+  },
+  {
+    "revision": "7fad4d0203c7d0ec1d24",
+    "url": "/js/chunk-17bc2355.69632d13.js"
+  },
+  {
+    "revision": "99df180639611d58d6bd",
+    "url": "/js/chunk-2d22d746.9cd2f2a6.js"
+  },
+  {
+    "revision": "37cd74c837945674e8d6",
+    "url": "/js/chunk-vendors.2f0ac98d.js"
+  },
+  {
+    "revision": "fbafddbca0454e2cf83c4640f9391b35",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
